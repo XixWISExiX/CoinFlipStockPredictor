004c74bc7b6f89e6e2b2ccf7f1787c83104353a5
Hugo Enrique Atayde <hugo.e.atayde@gmail.com>
2023-04-03 19:31:41 -0500

    Added a route for user model, not sure if needed

---
+++ customLogin/CFSP-app/backend/src/routes/User.ts
@@ -0,0 +1,24 @@
+import express from "express"
+import * as UserController from "../controllers/User"
+
+//*IMPORTANT*
+// Not entirely sure if this route for user is needed, but 
+//  i went ahead and added it just in case. Will probably need
+// to be reviewed
+
+
+/*
+    what this does is navigate through different groups
+    of objects in our database.
+*/ 
+
+// created new fuckin router
+// kinda iamgine like a mini app
+const router = express.Router()
+
+// self explanatory
+router.get("/", UserController.getModels)
+router.get("/:modelID", UserController.getModel)
+router.post("/", UserController.createModel)
+
+export default router
