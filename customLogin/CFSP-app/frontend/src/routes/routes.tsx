40ac259d03a467532eb04cc79922337a354b026d
hugo.e.atayde@gmail.com <hugoe@LAPTOP-FJ9SS67E>
2023-04-11 21:52:16 -0500

    add test route

--- customLogin/CFSP-app/frontend/src/routes/routes.tsx
+++ customLogin/CFSP-app/frontend/src/routes/routes.tsx
@@ -64,4 +64,8 @@ export default [
     element: lazyLoad(import("../pages/Home")),
     //element: lazyLoad(import("../../../../../StockChart/src/App.jsx")),
   },
+  // {
+  //   path: "/stock",
+  //   element: lazyLoad(import("../CoinFlipStockPredictor/StockChart/src/App"))
+  // },
 ] as Route[]
